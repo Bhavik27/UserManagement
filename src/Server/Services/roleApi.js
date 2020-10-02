fca1080e3ae70f93c437d5b80fbeb8fd8201688d
Bhavik27 <bhavik27071999@gmail.com>
2020-10-02 12:44:24 +0530

    role api created

---
+++ src/Server/Services/roleApi.js
@@ -0,0 +1,17 @@
+var express = require('express');
+const QueryFunction = require('../DBConnect');
+var app = express();
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
+
+app.get('/getRoles', (req, res) => {
+    QueryFunction('SELECT * FROM TBL_ROLE_MASTER', res)
+});
+
+module.exports = app;
