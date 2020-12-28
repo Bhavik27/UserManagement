bb96836316632f961d28dc493740fdcce5cbbffa
Bhavik27 <bhavik27071999@gmail.com>
2020-12-28 19:09:08 +0530

    no message

--- src/Server/DBConfig.js
+++ src/Server/DBConfig.js
@@ -1,9 +1,9 @@
 exports.config = {
-    user: '', //enter your username
-    password: '', //enter your password
+    user: 'admin', //enter your username
+    password: 'sa@12345', //enter your password
     database: 'UserMangementDB', //created DB name
-    server:'localhost',
-    port:1433,
+    server: 'localhost',
+    port: 1433,
     options: {
         encrypt: true,
         trustedConnection: true,
